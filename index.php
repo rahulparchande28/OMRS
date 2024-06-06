5fce849eb50a7da41166d680ea555d5048d6b5d3
Rahul Parchande <141058720+rahulparchande28@users.noreply.github.com>
2024-06-06 12:37:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Online Marriage Regitration System :: Home Page</title>
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--//Meta tag Keywords -->
+	<!--<--!<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/><!--stylesheet-->
+	<link rel="stylesheet" href="css/font-awesome.css"><!--font_aswesome_icons-->
+	<link href="//fonts.googleapis.com/css?family=Basic" rel="stylesheet"><!--online-fonts-->
+	<link href="//fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet"><!--online-fonts-->
+	<style>
+		body {
+ background-image: url("back.jpg");
+ background-color: #cccccc;
+}
+  
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+.topnav a.active {
+  background-color: #FB9337;
+  color: white;
+}
+p {
+  color: black;
+  text-indent: 1px;
+  text-transform: Lowcasecase;
+}
+  pre{color:green }
+  h1{color:orange}
+marquee { background-color:yellow }
+  </style>
+
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="admin/login.php">Admin</a>
+  <a href="user/login.php">User</a>
+</div>
+<div class="w3ls-head">
+		<h1 align='center'>Online Marriage Registration System </h1>
+		<!----<img src="nice1.jpeg" width="1450" height="760">--->
+</div>
+</body>
+</html>
